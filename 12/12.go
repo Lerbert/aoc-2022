a958038c2121ae850d7f69913f35ff47b0e1f161
Florian Lercher <florian.lercher@tum.de>
2022-12-12 14:34:11 +0100

    Add solution for day 12 part 2

--- 12/12.go
+++ 12/12.go
@@ -36,7 +36,7 @@ type node struct {
 	end     bool
 }
 
-func makeNode(height byte, x int, y int) node {
+func makeNode(height byte, x int, y int, end bool) node {
 	var intHeight int
 	if height == 'S' {
 		intHeight = 0
@@ -45,15 +45,15 @@ func makeNode(height byte, x int, y int) node {
 	} else {
 		intHeight = int(height - 'a')
 	}
-	return node{coord{x, y}, intHeight, false, height == 'E'}
+	return node{coord{x, y}, intHeight, false, end}
 }
 
-func bfs(heightMap *[][]node, init coord) int {
+func bfs(heightMap *[][]node, init coord, isEdge func(from *node, to *node) bool) int {
 	toVisit := map[coord]struct{}{init: {}}
-	return bfsRec(heightMap, toVisit)
+	return bfsRec(heightMap, toVisit, isEdge)
 }
 
-func bfsRec(heightMap *[][]node, toVisit map[coord]struct{}) int {
+func bfsRec(heightMap *[][]node, toVisit map[coord]struct{}, isEdge func(from *node, to *node) bool) int {
 	for steps := 0; len(toVisit) > 0 && steps < 1000; steps++ {
 		visitNext := make(map[coord]struct{}) // Use map to avoid duplicate insertions
 		for n := range toVisit {
@@ -65,7 +65,7 @@ func bfsRec(heightMap *[][]node, toVisit map[coord]struct{}) int {
 			// Look at all neighbors
 			for _, c := range n.neighbors(len((*heightMap)[0]), len(*heightMap)) {
 				neighborNode := &(*heightMap)[c.y][c.x]
-				if !neighborNode.visited && neighborNode.height <= curNode.height+1 {
+				if !neighborNode.visited && isEdge(curNode, neighborNode) {
 					visitNext[c] = struct{}{}
 				}
 			}
@@ -76,9 +76,18 @@ func bfsRec(heightMap *[][]node, toVisit map[coord]struct{}) int {
 	panic("")
 }
 
+func isEdge1(from *node, to *node) bool {
+	return to.height <= from.height+1
+}
+
+func isEdge2(from *node, to *node) bool {
+	return from.height <= to.height+1
+}
+
 func main() {
 	lines := inp.ReadLines("input")
 	heightMap := make([][]node, len(lines))
+
 	var init coord
 	for i, l := range lines {
 		sIndex := strings.Index(l, "S")
@@ -87,10 +96,22 @@ func main() {
 		}
 		heightMap[i] = make([]node, len(l))
 		for j, b := range []byte(l) {
-			heightMap[i][j] = makeNode(b, j, i)
+			heightMap[i][j] = makeNode(b, j, i, b == 'E')
 		}
 	}
-
-	stepsToE := bfs(&heightMap, init)
+	stepsToE := bfs(&heightMap, init, isEdge1)
 	fmt.Printf("Part 1: %d\n", stepsToE)
+
+	for i, l := range lines {
+		sIndex := strings.Index(l, "E")
+		if sIndex != -1 {
+			init = coord{sIndex, i}
+		}
+		heightMap[i] = make([]node, len(l))
+		for j, b := range []byte(l) {
+			heightMap[i][j] = makeNode(b, j, i, b == 'S' || b == 'a')
+		}
+	}
+	stepsToA := bfs(&heightMap, init, isEdge2)
+	fmt.Printf("Part 2: %d\n", stepsToA)
 }
