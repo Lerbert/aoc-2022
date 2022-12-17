ce23cf5007d29569d41552a83c788718838933db
Florian Lercher <florian.lercher@tum.de>
2022-12-17 16:04:51 +0100

    Add solution for day 17 part 2

--- 17/17.go
+++ 17/17.go
@@ -245,20 +245,6 @@ func squareCanLeft(grid [][GRID_WIDTH]bool, center util.Coord) bool {
 	return center.X > 0 && !util.Any(util.Map(grid[center.Y-1:center.Y+1], func(s [GRID_WIDTH]bool) bool { return s[center.X-1] }))
 }
 
-func printGrid(grid [][GRID_WIDTH]bool) {
-	for i := range grid {
-		line := grid[len(grid)-1-i]
-		for _, b := range line {
-			if b {
-				fmt.Print("#")
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println("")
-	}
-}
-
 func startPiece(shape int, highestPoint int, grid *[][GRID_WIDTH]bool) piece {
 	piece := piece{s: SHAPES[shape%NUM_SHAPES]}
 	top, left, bottom, _ := piece.edges()
@@ -269,27 +255,92 @@ func startPiece(shape int, highestPoint int, grid *[][GRID_WIDTH]bool) piece {
 	return piece
 }
 
+func gridEquals(g1 [][GRID_WIDTH]bool, g2 [][GRID_WIDTH]bool) bool {
+	if len(g1) != len(g2) {
+		return false
+	}
+
+	for i := range g1 {
+		for j := range g1[i] {
+			if g1[i][j] != g2[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func gridSubset(sub [][GRID_WIDTH]bool, super [][GRID_WIDTH]bool) bool {
+	if len(sub) > len(super) {
+		return false
+	}
+
+	for i := range sub {
+		for j := range sub[i] {
+			if sub[i][j] && !super[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+type tracker struct {
+	jetIdx int
+	s      shape
+}
+
+type trackerVal struct {
+	height       int
+	stoppedRocks int
+}
+
 func simulate(jets []byte, untilRocksStopped int) int {
 	lenJets := len(jets)
 	stoppedPieces := 0
+	gridBottom := 0
 	highestPoint := -1
 	needNewPiece := true
 	grid := make([][GRID_WIDTH]bool, 5)
+	fallen := 0
+	maxFallen := 0
+	memo := make(map[tracker]trackerVal)
 	var currentPiece piece
 	for step := 0; stoppedPieces < untilRocksStopped; step++ {
 		if needNewPiece {
-			currentPiece = startPiece(stoppedPieces, highestPoint, &grid)
+			currentPiece = startPiece(stoppedPieces, highestPoint-gridBottom, &grid)
+			fallen = 0
 			needNewPiece = false
 		}
 		if step%2 != 0 {
 			// Fall
 			couldFall := currentPiece.fall(grid)
+			fallen++
 			if !couldFall {
 				currentPiece.settle(grid)
 				stoppedPieces++
 				needNewPiece = true
 				top, _, _, _ := currentPiece.edges()
-				highestPoint = util.Max(currentPiece.center.Y+top.Y, highestPoint)
+				highestPoint = util.Max(currentPiece.center.Y+top.Y+gridBottom, highestPoint)
+				maxFallen = util.Max(fallen-1, maxFallen)
+
+				prev, ok := memo[tracker{jetIdx: (step / 2) % lenJets, s: currentPiece.s}]
+				if ok {
+					cycleHeight := highestPoint - prev.height
+					cycleRocks := stoppedPieces - prev.stoppedRocks
+					unchanged := highestPoint - maxFallen + 1 // add 1 because slice end is excluded
+					// Rows under maxFallen won't change, so check if those are duplicated; above unchanged must be a subset of the pattern, i.e., pattern must be blocked, where it is blocked
+					if unchanged-2*cycleHeight >= 0 && gridEquals(grid[unchanged-cycleHeight:unchanged], grid[unchanged-2*cycleHeight:unchanged-cycleHeight]) && gridSubset(grid[unchanged:], grid[unchanged-cycleHeight:unchanged]) {
+						fmt.Printf("Cycle detected after %d rocks fell\n", stoppedPieces)
+						fmt.Printf("In each cycle %d rocks fall, increasing the maximum height by %d\n", cycleRocks, cycleHeight)
+						cyclesToGo := (untilRocksStopped - stoppedPieces) / cycleRocks
+						fmt.Printf("Fast-forwarding %d cycles\n", cyclesToGo)
+						highestPoint += cyclesToGo * cycleHeight
+						gridBottom += cyclesToGo * cycleHeight
+						stoppedPieces += cyclesToGo * cycleRocks
+					}
+				}
+				memo[tracker{jetIdx: (step / 2) % lenJets, s: currentPiece.s}] = trackerVal{height: highestPoint, stoppedRocks: stoppedPieces}
 			}
 		} else {
 			// Push
@@ -303,12 +354,30 @@ func simulate(jets []byte, untilRocksStopped int) int {
 			}
 		}
 	}
-	return highestPoint
+	return highestPoint + 1
 }
+
+// func printGrid(grid [][GRID_WIDTH]bool) {
+// 	for i := range grid {
+// 		line := grid[len(grid)-1-i]
+// 		for _, b := range line {
+// 			if b {
+// 				fmt.Print("#")
+// 			} else {
+// 				fmt.Print(".")
+// 			}
+// 		}
+// 		fmt.Println("")
+// 	}
+// }
 
 func main() {
 	lines := inp.ReadLines("input")
+	jets := []byte(lines[0])
 
-	highestPoint := simulate([]byte(lines[0]), 2022) + 1
+	highestPoint := simulate(jets, 2022)
 	fmt.Printf("Part 1: %d\n", highestPoint)
+
+	highestPoint = simulate(jets, 1000000000000)
+	fmt.Printf("Part 2: %d\n", highestPoint)
 }
