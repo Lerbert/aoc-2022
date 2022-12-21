907d2d40f73ee4cf347fad0925c59ccd072dfa12
Florian Lercher <florian.lercher@tum.de>
2022-12-21 11:20:35 +0100

    Add solution for day 21 part 2

--- 21/21.go
+++ 21/21.go
@@ -84,6 +84,89 @@ func findMonkeyNumberRec(monkeys map[string]monkey, currentMonkeyName string, me
 	return n
 }
 
+// func hasHumn(monkeys map[string]monkey, name string) bool {
+// 	memo := make(map[string]bool)
+// 	return hasHumnRec(monkeys, name, memo)
+// }
+
+func hasHumnRec(monkeys map[string]monkey, currentMonkeyName string, memo map[string]bool) bool {
+	b, ok := memo[currentMonkeyName]
+	if ok {
+		return b
+	}
+
+	currentMonkey := monkeys[currentMonkeyName]
+	if currentMonkey.op == Immediate {
+		b = currentMonkeyName == HUMN
+	} else {
+		b = hasHumnRec(monkeys, currentMonkey.lhs, memo) || hasHumnRec(monkeys, currentMonkey.rhs, memo)
+	}
+
+	memo[currentMonkeyName] = b
+	return b
+}
+
+func findHumnNumber(monkeys map[string]monkey, start string) int {
+	memoNumbers := make(map[string]int)
+	memoHasHumn := make(map[string]bool)
+	startMonkey := monkeys[start]
+	var matchNumber int
+	var monkey string
+	if hasHumnRec(monkeys, startMonkey.lhs, memoHasHumn) {
+		matchNumber = findMonkeyNumberRec(monkeys, startMonkey.rhs, memoNumbers)
+		monkey = startMonkey.lhs
+	} else {
+		matchNumber = findMonkeyNumberRec(monkeys, startMonkey.lhs, memoNumbers)
+		monkey = startMonkey.rhs
+	}
+	return findHumnNumberRec(monkeys, monkey, matchNumber, memoNumbers, memoHasHumn)
+}
+
+func findHumnNumberRec(monkeys map[string]monkey, currentMonkeyName string, matchNumber int, memoNumbers map[string]int, memoHasHumn map[string]bool) int {
+	currentMonkey := monkeys[currentMonkeyName]
+
+	if currentMonkey.op == Immediate {
+		if currentMonkeyName == HUMN {
+			return matchNumber
+		} else {
+			return currentMonkey.immediate
+		}
+	}
+
+	// humn is queried exactly once, so it must appear in exactly one of lhs and rhs
+	humn, noHumn := currentMonkey.lhs, currentMonkey.rhs
+	if hasHumnRec(monkeys, noHumn, memoHasHumn) {
+		humn, noHumn = noHumn, humn
+	}
+
+	noHumnNumber := findMonkeyNumberRec(monkeys, noHumn, memoNumbers)
+	var nextMatchNumber int
+	switch currentMonkey.op {
+	case Add:
+		nextMatchNumber = matchNumber - noHumnNumber
+	case Mul:
+		nextMatchNumber = matchNumber / noHumnNumber
+	case Sub:
+		if humn == currentMonkey.lhs {
+			nextMatchNumber = matchNumber + noHumnNumber
+		} else {
+			nextMatchNumber = noHumnNumber - matchNumber
+		}
+	case Div:
+		if humn == currentMonkey.lhs {
+			nextMatchNumber = matchNumber * noHumnNumber
+		} else {
+			nextMatchNumber = noHumnNumber / matchNumber
+		}
+	}
+	return findHumnNumberRec(monkeys, humn, nextMatchNumber, memoNumbers, memoHasHumn)
+}
+
+const (
+	ROOT = "root"
+	HUMN = "humn"
+)
+
 func main() {
 	lines := inp.ReadLines("input")
 	monkeys := make(map[string]monkey)
@@ -92,6 +175,9 @@ func main() {
 		monkeys[m.name] = m
 	}
 
-	rootNumber := findMonkeyNumber(monkeys, "root")
+	rootNumber := findMonkeyNumber(monkeys, ROOT)
 	fmt.Printf("Part 1: %d\n", rootNumber)
+
+	humnNumber := findHumnNumber(monkeys, ROOT)
+	fmt.Printf("Part 2: %d\n", humnNumber)
 }
