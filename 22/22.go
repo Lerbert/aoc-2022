11bea077b42d48d3beb57df44bfe617061d3c090
Florian Lercher <florian.lercher@tum.de>
2022-12-22 14:31:49 +0100

    Add solution for day 22 part 2

--- 22/22.go
+++ 22/22.go
@@ -4,6 +4,7 @@ import (
 	util "aoc2022/aoc"
 	inp "aoc2022/input"
 	"fmt"
+	"log"
 	"regexp"
 )
 
@@ -55,6 +56,7 @@ func (f facing) score() int {
 
 type instruction interface {
 	execute(board [][]marker, transposedBoard [][]marker, pos util.Coord, f facing) (util.Coord, facing)
+	execute2(board [][]marker, transposedBoard [][]marker, pos util.Coord, f facing) (util.Coord, facing)
 }
 
 type turn int
@@ -73,6 +75,10 @@ func (t turn) execute(board [][]marker, transposedBoard [][]marker, pos util.Coo
 	default:
 		panic("Unknown turn")
 	}
+}
+
+func (t turn) execute2(board [][]marker, transposedBoard [][]marker, pos util.Coord, f facing) (util.Coord, facing) {
+	return t.execute(board, transposedBoard, pos, f)
 }
 
 func turnFromByte(b byte) turn {
@@ -153,21 +159,182 @@ func forwardInStraight(straight []marker, pos int, cnt int) int {
 	return pos
 }
 
-var turnRegex = regexp.MustCompile(`R|L`)
-var forwardRegex = regexp.MustCompile(`\d+`)
-
-func parseInstructions(s string) []instruction {
-	turns := util.Map(turnRegex.FindAllString(s, -1), func(m string) turn { return turnFromByte(m[0]) })
-	forwards := util.Map(forwardRegex.FindAllString(s, -1), func(m string) forward { return forward(inp.MustAtoi(m)) })
-	instructions := make([]instruction, len(turns)+len(forwards))
-	for i := range instructions {
-		if i%2 == 0 {
-			instructions[i] = forwards[i/2]
-		} else {
-			instructions[i] = turns[i/2]
+func (fw forward) execute2(board [][]marker, transposedBoard [][]marker, pos util.Coord, f facing) (util.Coord, facing) {
+	cnt := int(fw)
+	var lastPos util.Coord
+	var lastF facing
+	for i := 0; i < cnt; i++ {
+		lastPos = pos
+		lastF = f
+		pos = incrementCoord(pos, f)
+		pos, f = wrapCube(board, pos, f, lastPos)
+		if board[pos.Y][pos.X] == Wall {
+			return lastPos, lastF
 		}
 	}
-	return instructions
+	return pos, f
+}
+
+func incrementCoord(pos util.Coord, f facing) util.Coord {
+	switch f {
+	case North:
+		return util.Coord{X: pos.X, Y: pos.Y - 1}
+	case West:
+		return util.Coord{X: pos.X - 1, Y: pos.Y}
+	case South:
+		return util.Coord{X: pos.X, Y: pos.Y + 1}
+	case East:
+		return util.Coord{X: pos.X + 1, Y: pos.Y}
+	default:
+		panic("Unknown direction")
+	}
+}
+
+func wrapCube(board [][]marker, pos util.Coord, f facing, lastPos util.Coord) (util.Coord, facing) {
+	width, height := len(board[0]), len(board)
+	if pos.X < 0 || pos.X >= width || pos.Y < 0 || pos.Y >= height || board[pos.Y][pos.X] == Wrap {
+		return leave(currentFace(lastPos), lastPos, f)
+	} else {
+		return pos, f
+	}
+}
+
+func transpose[T interface{}](m [][]T) [][]T {
+	transposed := make([][]T, len(m[0]))
+	for col := range transposed {
+		transposed[col] = make([]T, len(m))
+		for row := range transposed[col] {
+			transposed[col][row] = m[row][col]
+		}
+	}
+	return transposed
+}
+
+type cubeFace int
+
+const (
+	Top cubeFace = iota
+	Bottom
+	Front
+	Back
+	LeftFace
+	RightFace
+	NUM_FACES int = iota
+)
+
+// Net
+//
+//	TE
+//	S
+//
+// WB
+// N
+var (
+	TOP_FACE     = util.Coord{X: 50, Y: 0}
+	BOTTOM_FACE  = util.Coord{X: 50, Y: 100}
+	FRONT_FACE   = util.Coord{X: 50, Y: 50} // South
+	BACK_FACE    = util.Coord{X: 0, Y: 150} // North
+	LEFT_FACE    = util.Coord{X: 0, Y: 100} // West
+	RIGHT_FACE   = util.Coord{X: 100, Y: 0} // East
+	FACE_CORNERS = [NUM_FACES]util.Coord{TOP_FACE, BOTTOM_FACE, FRONT_FACE, BACK_FACE, LEFT_FACE, RIGHT_FACE}
+)
+
+const CUBE_SIZE = 50
+
+func currentFace(pos util.Coord) cubeFace {
+	if TOP_FACE.X <= pos.X && pos.X < TOP_FACE.X+CUBE_SIZE && TOP_FACE.Y <= pos.Y && pos.Y < TOP_FACE.Y+CUBE_SIZE {
+		return Top
+	} else if BOTTOM_FACE.X <= pos.X && pos.X < BOTTOM_FACE.X+CUBE_SIZE && BOTTOM_FACE.Y <= pos.Y && pos.Y < BOTTOM_FACE.Y+CUBE_SIZE {
+		return Bottom
+	} else if FRONT_FACE.X <= pos.X && pos.X < FRONT_FACE.X+CUBE_SIZE && FRONT_FACE.Y <= pos.Y && pos.Y < FRONT_FACE.Y+CUBE_SIZE {
+		return Front
+	} else if BACK_FACE.X <= pos.X && pos.X < BACK_FACE.X+CUBE_SIZE && BACK_FACE.Y <= pos.Y && pos.Y < BACK_FACE.Y+CUBE_SIZE {
+		return Back
+	} else if LEFT_FACE.X <= pos.X && pos.X < LEFT_FACE.X+CUBE_SIZE && LEFT_FACE.Y <= pos.Y && pos.Y < LEFT_FACE.Y+CUBE_SIZE {
+		return LeftFace
+	} else if RIGHT_FACE.X <= pos.X && pos.X < RIGHT_FACE.X+CUBE_SIZE && RIGHT_FACE.Y <= pos.Y && pos.Y < RIGHT_FACE.Y+CUBE_SIZE {
+		return RightFace
+	} else {
+		log.Fatal("Position is not on cube ", pos)
+		panic("")
+	}
+}
+
+func relativeCoord(face cubeFace, pos util.Coord) util.Coord {
+	return util.Coord{X: pos.X - FACE_CORNERS[face].X, Y: pos.Y - FACE_CORNERS[face].Y}
+}
+
+// func normal(from cubeFace, to cubeFace, pos util.Coord) util.Coord {
+// 	relative := relativeCoord(from, pos)
+// 	return util.Coord{X: FACE_CORNERS[to].X + relative.X, Y: FACE_CORNERS[to].Y + relative.Y}
+// }
+
+func crossover(from cubeFace, to cubeFace, pos util.Coord) util.Coord {
+	relative := relativeCoord(from, pos)
+	return util.Coord{X: FACE_CORNERS[to].X + relative.Y, Y: FACE_CORNERS[to].Y + relative.X}
+}
+
+// func invertedX(from cubeFace, to cubeFace, pos util.Coord) util.Coord {
+// 	relative := relativeCoord(from, pos)
+// 	return util.Coord{X: FACE_CORNERS[to].X + CUBE_SIZE - 1 - relative.X, Y: FACE_CORNERS[to].Y + relative.Y}
+// }
+
+func invertedY(from cubeFace, to cubeFace, pos util.Coord) util.Coord {
+	relative := relativeCoord(from, pos)
+	return util.Coord{X: FACE_CORNERS[to].X + relative.X, Y: FACE_CORNERS[to].Y + CUBE_SIZE - 1 - relative.Y}
+}
+
+func leave(face cubeFace, pos util.Coord, f facing) (util.Coord, facing) {
+	switch face {
+	case Top:
+		switch f {
+		case West:
+			return invertedY(Top, LeftFace, pos), East
+		case North:
+			return crossover(Top, Back, pos), East
+		}
+	case Bottom:
+		switch f {
+		case South:
+			return crossover(Bottom, Back, pos), West
+		case East:
+			return invertedY(Bottom, RightFace, pos), West
+		}
+	case Front:
+		switch f {
+		case West:
+			return crossover(Front, LeftFace, pos), South
+		case East:
+			return crossover(Front, RightFace, pos), North
+		}
+	case Back:
+		switch f {
+		case East:
+			return crossover(Back, Bottom, pos), North
+		case South:
+			return invertedY(Back, RightFace, pos), South
+		case West:
+			return crossover(Back, Top, pos), South
+		}
+	case LeftFace:
+		switch f {
+		case North:
+			return crossover(LeftFace, Front, pos), East
+		case West:
+			return invertedY(LeftFace, Top, pos), East
+		}
+	case RightFace:
+		switch f {
+		case North:
+			return invertedY(RightFace, Back, pos), North
+		case East:
+			return invertedY(RightFace, Bottom, pos), West
+		case South:
+			return crossover(RightFace, Front, pos), West
+		}
+	}
+	log.Fatal("Transition should not be possible ", face, " ", f)
+	panic("")
 }
 
 func startPos(board [][]marker) (util.Coord, facing) {
@@ -188,15 +355,34 @@ func followInstructions(board [][]marker, transposedBoard [][]marker, startC uti
 	return pos, f
 }
 
-func transpose[T interface{}](m [][]T) [][]T {
-	transposed := make([][]T, len(m[0]))
-	for col := range transposed {
-		transposed[col] = make([]T, len(m))
-		for row := range transposed[col] {
-			transposed[col][row] = m[row][col]
+func followInstructions2(board [][]marker, transposedBoard [][]marker, startC util.Coord, startF facing, instructions []instruction) (util.Coord, facing) {
+	pos := startC
+	f := startF
+	for _, instr := range instructions {
+		pos, f = instr.execute2(board, transposedBoard, pos, f)
+	}
+	return pos, f
+}
+
+var turnRegex = regexp.MustCompile(`R|L`)
+var forwardRegex = regexp.MustCompile(`\d+`)
+
+func parseInstructions(s string) []instruction {
+	turns := util.Map(turnRegex.FindAllString(s, -1), func(m string) turn { return turnFromByte(m[0]) })
+	forwards := util.Map(forwardRegex.FindAllString(s, -1), func(m string) forward { return forward(inp.MustAtoi(m)) })
+	instructions := make([]instruction, len(turns)+len(forwards))
+	for i := range instructions {
+		if i%2 == 0 {
+			instructions[i] = forwards[i/2]
+		} else {
+			instructions[i] = turns[i/2]
 		}
 	}
-	return transposed
+	return instructions
+}
+
+func password(pos util.Coord, f facing) int {
+	return 1000*(pos.Y+1) + 4*(pos.X+1) + f.score()
 }
 
 func main() {
@@ -219,5 +405,8 @@ func main() {
 
 	startC, startF := startPos(board)
 	pos, f := followInstructions(board, transposedBoard, startC, startF, instructions)
-	fmt.Printf("Part 1: %d\n", 1000*(pos.Y+1)+4*(pos.X+1)+f.score())
+	fmt.Printf("Part 1: %d\n", password(pos, f))
+
+	pos, f = followInstructions2(board, transposedBoard, startC, startF, instructions)
+	fmt.Printf("Part 2: %d\n", password(pos, f))
 }
